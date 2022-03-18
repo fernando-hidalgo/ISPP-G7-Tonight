55c24e6b65ccd6220631653d9ca8a15c67b60ea2
alebangon <alebangon@alum.us.es>
2022-03-18 20:02:46 +0100

    Commit inicial Github Actions
    Commit inicial de configuración de Github Actions para la integración continua

--- tonight/proyecto/tests.py
+++ tonight/proyecto/tests.py
@@ -1,3 +1,7 @@
+from typing_extensions import Self
 from django.test import TestCase
 
 # Create your tests here.
+class ToNightTestCase(TestCase):
+    def test_mock():
+        Self.assertEquals(1,1)
