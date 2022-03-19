a20e72e69de1e7cdad49496c97c08a0eb6f875c2
Álvaro Paradas Borego <alvaro_cabu@hotmail.es>
2022-03-20 00:23:24 +0100

    venta de entrada estructura

--- tonight/proyecto/admin.py
+++ tonight/proyecto/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+def venderEntradas(self, request, *args, **kwargs):
+    #print("hola mundo")
+    # asignamos una entrada disponible a un usuario
+    nombreEvento = input("evento al que quiero ir:")
+    evento_list = request.data.get('eventos') #conjunto de todos los eventos
+    for e in evento_list:
+        if(nombreEvento == e.nombre):
+            evento = request.data.get('e') # nos quedamos con el evento coincidente
+            entradas = request.data.get('entrada') # conjunto de todas las entradas
+            for en in entradas:
+                if en.evento == evento:
+                    entradaTratada = en
+                    if entradaTratada.estado == 'STATUS.E'
+                    entradaAdquirida = en
+                    # cargar los campos de entrada
+
+    
