0aaca58e17a3ab7e5fd5f3e32110bed66cba6318
Álvaro Paradas Borego <alvaro_cabu@hotmail.es>
2022-03-22 17:47:35 +0100

    TN7: Fix + vistas

--- tonight/proyecto/compra_venta.py
+++ tonight/proyecto/compra_venta.py
@@ -5,14 +5,35 @@ from django.contrib.auth.models import User
 from tonight.proyecto.models import Cliente, Entrada, Evento
 
 # asignamos una entrada disponible a un usuario
-def venderEntradas(request, nombre_evento):
+def conprarEntradas(request, nombre_evento):
     evento_ = Evento.objects.get(nombre=nombre_evento) # evento del que queremos entrada
     usuario = request.user # usuario que compra la entrada
-    entradas = Entrada.objects.filter(evento=evento_).all() # conjunto de todas las entradas
-    entrada_disponible = entradas.filter(estado='E')
-    #modificamos los valores de la entrada a vender
-    entrada_disponible.fechaCompra=datetime.today
-    entrada_disponible.estado='V'
-    entrada_disponible.cliente=usuario
-    # guardamos los datos modificados en la base de datos
-    entrada_disponible.save()
+    #comprobamos que haya entradas
+    entradas = Entrada.objects.filter(evento=evento_).all()     
+    entradas_vendidas = entradas.filter(estado='V').count()
+    
+    if evento_.totalEntradas > entradas_vendidas:
+        entrada_disponible = entradas.filter(estado='E')
+        #modificamos los valores de la entrada a vender
+        entrada_disponible.fechaCompra=datetime.today
+        entrada_disponible.cliente=usuario
+        entrada_disponible.estado('V')
+        # guardamos los datos modificados en la base de datos
+        entrada_disponible.save()
+
+    # asignamos una entrada disponible a un usuario
+def venderEntradas(request, entrada_id):
+    entrada = Entrada.objects.get(id=entrada_id)
+    usuario = request.user
+    if entrada.cliente == usuario:
+        entrada.estado('E')
+        entrada.save()
+    
+    
+def cancelarVentaEntrada(request, entrada_id):
+    entrada = Entrada.objects.get(id=entrada_id)
+    usuario = request.user
+    if entrada.cliente == usuario:
+        entrada.estado('V')
+        entrada.save()
+
