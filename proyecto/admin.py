531d42e6f765aa5ef78aa859ddbbe5a8b09eb64e
Alejandro Baños González <alebangon@alum.us.es>
2022-03-29 16:33:25 +0200

    Arreglado import

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Cliente,Evento,Entrada,Empleado,Empresa
+from proyecto.models import Cliente,Evento,Entrada,Empleado,Empresa
 # Register your models here.
 admin.site.register(Evento)
 admin.site.register(Cliente)
