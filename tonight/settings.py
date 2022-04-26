deed676bf497def531ce5a8f716ee76d6e8e39aa
Alejandro Baños González <alebangon@alum.us.es>
2022-04-26 22:17:14 +0200

    fix de staticfiles para despliegue en heroku

--- tonight/settings.py
+++ tonight/settings.py
@@ -140,6 +140,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
